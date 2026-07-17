388b9a925dfdfa69f751700e41149773947ac4fb
itzhakfilgut-del <itzhakfilgut@gmail.com>
2026-07-17 11:07:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ export default {
         return new Response("Invalid JSON", { status: 400 });
       }
       
-      console.log("MESSAGE DATA: " + JSON.stringify(body.messageData));
+      console.log(JSON.stringify(body));
       console.log(JSON.stringify(body.messageData, null, 2));
       // מטפלים רק בהודעות נכנסות
       if (body?.typeWebhook !== "incomingMessageReceived") {
